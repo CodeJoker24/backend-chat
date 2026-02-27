c470ef13a193b0bc176ff21695ce667ba963e1dc
CodeJoker24 <damilaremishael@gmail.com>
2026-02-26 17:41:17 -0800

    updated

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ const PORT = process.env.PORT || 4000;
 const authRoutes = require("./routes/auth_routes")
 const messageRoutes = require("./routes/messages")
 
+// const allowOrigins = [
+//     "http://localhost:5173/",
+//     "https://frontend-chat-orcin.vercel.app/"
+// ]
+
+
 app.use(cors());
+// app.options("", cors())
 app.use(express.json());
 app.use("/api/auth_routes", authRoutes)
 app.use("/api/messages", messageRoutes)
